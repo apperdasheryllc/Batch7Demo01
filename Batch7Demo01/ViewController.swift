f9a548df60bae312c2ed95e14e50328f4efc10c6
gdiehl <graham@apperdashery.net>
2022-04-11 10:18:07 -0400

    removed storyboard to use code only

--- Batch7Demo01/ViewController.swift
+++ Batch7Demo01/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func configureView(){
+        self.view.backgroundColor = .systemBackground
+        
+        let point = CGPoint.zero
+        let size = CGSize(width: 100, height: 50)
+        let rect = CGRect(origin: point, size: size)
+        let textLabel = UILabel(frame: rect)
+        textLabel.center = view.center
+        textLabel.text = "Hello World"
+        textLabel.textAlignment = .center
+        textLabel.backgroundColor = .secondarySystemBackground
+        view.addSubview(textLabel)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureView()
     }
 
 
